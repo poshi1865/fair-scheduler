b5fe7cc64c8b5ac51a0e171c75a4dcf6a6a293ce
naachiket <naachiket.dev@gmail.com>
2025-11-04 21:13:42 +0530

    Add more tests

--- src/fair_scheduler.rs
+++ src/fair_scheduler.rs
@@ -30,6 +30,7 @@ impl PartialEq for Task {
 pub struct FairScheduler {
     system_capacity: usize,
     user_tasks_map: HashMap<String, VecDeque<Task>>,
+    current_task_count: usize
 }
 
 impl FairScheduler {
@@ -37,6 +38,7 @@ impl FairScheduler {
         FairScheduler {
             system_capacity: system_capacity,
             user_tasks_map: HashMap::new(),
+            current_task_count: 0
         }
     }
 
@@ -61,6 +63,7 @@ impl FairScheduler {
                 }
 
                 final_task_list.push(task_list.pop_front().unwrap());
+                self.current_task_count -= 1;
                 last_iteration_useful = true;
 
                 tasks_to_send_count -= 1;
@@ -73,17 +76,22 @@ impl FairScheduler {
         return final_task_list;
     }
 
-    pub fn add_task(&mut self, user_id: &String, task: Task) {
+    pub fn add_task(&mut self, user_id: String, task: Task) {
         // A new task is always pushed to the end of the list.
         // This means that tasks that have been waiting the longest will always be at the
         // front.
-        if self.user_tasks_map.contains_key(user_id) {
-            let mut task_queue: &mut VecDeque<Task> = self.user_tasks_map.get_mut(user_id).unwrap();
+        if self.user_tasks_map.contains_key(&user_id) {
+            let mut task_queue: &mut VecDeque<Task> = self.user_tasks_map.get_mut(&user_id).unwrap();
             task_queue.push_back(task);
         }
         else {
             self.user_tasks_map.insert(user_id.clone(), VecDeque::from([task]));
         }
+        self.current_task_count += 1;
+    }
+
+    pub fn get_current_task_count(&mut self) -> usize {
+        return self.current_task_count;
     }
 }
 
@@ -95,13 +103,44 @@ mod tests {
     fn test_add_task() {
         let mut fs: FairScheduler = FairScheduler::new(100);
         let task = Task::new("1".to_string(), "user1".to_string(), 0.3);
-        fs.add_task(&"user1".to_string(), task);
+        fs.add_task("user1".to_string(), task);
 
         let test_task = Task::new("1".to_string(), "user1".to_string(), 0.3);
         let final_task_list = fs.run_cycle(0);
         for t in final_task_list {
-            println!("Hmmm");
             assert_eq!(test_task, t);
         }
+    }
+
+    #[test]
+    fn test_task_consumption() {
+        let mut fs: FairScheduler = FairScheduler::new(100);
+
+        for i in 0..100 {
+            let user_id = format!("user{}", i);
+            let task = Task::new(i.to_string(), user_id.clone(), 0.5);
+            fs.add_task(user_id, task);
+        }
+
+        assert_eq!(fs.get_current_task_count(), 100);
+
+        let mut output_task_list = fs.run_cycle(40);
+
+        assert_eq!(fs.get_current_task_count(), 40);
+        assert_eq!(output_task_list.len(), 60);
+
+        for i in 100..200 {
+            let user_id = format!("user1");
+            let task = Task::new(i.to_string(), user_id.clone(), 0.5);
+            fs.add_task(user_id, task);
+        }
+
+        output_task_list = fs.run_cycle(50);
+        assert_eq!(fs.get_current_task_count(), 90);
+        assert_eq!(output_task_list.len(), 50);
+
+        output_task_list = fs.run_cycle(0);
+        assert_eq!(fs.get_current_task_count(), 0);
+        assert_eq!(output_task_list.len(), 90);
     }
 }
