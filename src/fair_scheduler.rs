f7714fe5b9a9d425984c930d7143bed7aa1364e7
naachiket <naachiket.dev@gmail.com>
2025-11-07 11:50:44 +0530

    Account for cycles waiting for each user

--- src/fair_scheduler.rs
+++ src/fair_scheduler.rs
@@ -56,34 +56,38 @@ impl FairScheduler {
     }
 
     pub fn run_cycle(&mut self, current_system_usage: usize) -> Vec<Task> {
-        let mut tasks_to_send_count = self.system_capacity - current_system_usage;
+        let mut max_tasks_can_send = self.system_capacity - current_system_usage;
         let mut final_task_list: Vec<Task> = Vec::new();
 
-        let mut last_iteration_useful: bool = true;
+        // Find out n-c users that have been waiting the longest. Then iterate through them and
+        // get tasks
+        let mut sorted_list: Vec<(&String, &mut User)> = Vec::new();
+        for (id, user) in self.users.iter_mut() {
+            sorted_list.push((id, user));
+        }
+        // sorted_list.sort_by(|a, b| b.1.cycles_waiting.cmp(&a.1.cycles_waiting));
+        sorted_list.sort_by_key(|(_, user)| std::cmp::Reverse(user.cycles_waiting));
 
-        while tasks_to_send_count > 0 && last_iteration_useful {
-            // Find out n-c users that have been waiting the longest. Then iterate through them and
-            // get tasks
-            let mut sorted_list: Vec<(&String, &mut User)> = Vec::new();
-            for (id, user) in self.users.iter_mut() {
-                sorted_list.push((id, user));
-            }
-            sorted_list.sort_by(|a, b| b.1.cycles_waiting.cmp(&a.1.cycles_waiting));
-
+        let mut all_users_served = false;
+        let mut last_iteration_useful = true;
+        while max_tasks_can_send != 0 && last_iteration_useful {
             last_iteration_useful = false;
             for (_, user) in sorted_list.iter_mut() {
                 if user.task_list.len() == 0 {
                     continue;
                 }
-                final_task_list.push(user.task_list.pop_front().unwrap());
-                self.current_task_count -= 1;
-                last_iteration_useful = true;
-
-                tasks_to_send_count -= 1;
-                if tasks_to_send_count == 0 {
-                    break;
+                if max_tasks_can_send == 0 && !all_users_served {
+                    user.cycles_waiting += 1;
+                }
+                else {
+                    user.cycles_waiting = 0;
+                    final_task_list.push(user.task_list.pop_front().unwrap());
+                    self.current_task_count -= 1;
+                    max_tasks_can_send -= 1;
+                    last_iteration_useful = true;
                 }
             }
+            all_users_served = true;
         }
 
         return final_task_list;
@@ -140,32 +144,54 @@ mod tests {
 
         let test_task = Task::new("1".to_string(), 0.3);
         let final_task_list = fs.run_cycle(0);
-        for t in final_task_list {
-            assert_eq!(test_task, t);
-        }
+        assert_eq!(final_task_list[0], test_task);
     }
 
     #[test]
     fn test_all_users_equal() {
-        let N: usize = 25;
+        let n: usize = 25;
 
-        let mut fs: FairScheduler = FairScheduler::new(N);
+        let mut fs: FairScheduler = FairScheduler::new(n);
 
-        for i in 0..N {
+        for i in 0..25 {
+            let user_id = format!("user{}", i);
+            let task = Task::new(i.to_string(), 0.5);
+            fs.add_task(user_id, task);
+        }
+        for i in 25..50 {
+            let user_id = format!("user{}", i);
+            let task = Task::new(i.to_string(), 0.5);
+            fs.add_task(user_id, task);
+        }
+        for i in 50..100 {
             let user_id = format!("user{}", i);
             let task = Task::new(i.to_string(), 0.5);
             fs.add_task(user_id, task);
         }
 
-        assert_eq!(fs.get_current_task_count(), N);
+        let output_task_list = fs.run_cycle(0);
+        assert_eq!(output_task_list.len(), 25);
 
-        let mut output_task_list = fs.run_cycle(10);
+        // After 1 cycle, 25 tasks should be done
+        let mut count_not_done = 0;
+        for (_, user) in fs.users.iter() {
+            if user.cycles_waiting == 1 {
+                assert_eq!(user.task_list.len(), 1);
+                count_not_done += 1;
+            }
+        }
+        assert_eq!(count_not_done, 75);
 
-        assert_eq!(fs.get_current_task_count(), 10);
-        assert_eq!(output_task_list.len(), 15);
+        fs.run_cycle(0);
 
-        fs.run_cycle(5);
-
+        count_not_done = 0;
+        for (_, user) in fs.users.iter() {
+            if user.cycles_waiting == 2 {
+                assert_eq!(user.task_list.len(), 1);
+                count_not_done += 1;
+            }
+        }
+        assert_eq!(count_not_done, 50);
     }
 
     #[test]
